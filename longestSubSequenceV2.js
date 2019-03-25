07b936d9888fd8527a6a1dbc277ee578d668e54d
Steven Duncan <steven.k.duncan@gmail.com>
2019-03-24 18:14:12 -0600

    test new grids

--- longestSubSequenceV2.js
+++ longestSubSequenceV2.js
@@ -26,6 +26,9 @@ Examples/Edge Cases:
 
 // -Start of Code-                                                 ===
 const longestSequence = (grid) => {
+  if (grid === [] || !grid) {
+    return 0;
+  }
   const gHeight = grid.length;
   const gWidth = grid[0].length;
   let longest = 0;
@@ -179,7 +182,7 @@ const longestSequence = (grid) => {
       longest = tempLongest > longest ? tempLongest : longest;
     }
   }
-  console.log(recurseCount);
+  // console.log(recurseCount);
   return longest;
 };
 // -End of Code-                                                   ===
@@ -277,12 +280,14 @@ const longestSequence = (grid) => {
 
 
 const test10 = [ 
-  [ 1, 1, 8, 0, 3, 0 ],
-  [ 7, 4, 3, 4, 4, 1 ],
-  [ 5, 7, 4, 6, 8, 0 ],
-  [ 1, 3, 3, 7, 1, 7 ],
-  [ 3, 8, 3, 0, 7, 7 ],
-  [ 0, 6, 1, 1, 7, 6 ] 
+  [ 6, 2, 4, 1, 2, 5, 4, 4 ],
+  [ 7, 6, 4, 5, 1, 5, 2, 5 ],
+  [ 7, 2, 2, 1, 2, 4, 6, 3 ],
+  [ 0, 0, 8, 8, 4, 0, 1, 8 ],
+  [ 1, 7, 0, 2, 6, 3, 7, 6 ],
+  [ 7, 8, 5, 4, 7, 5, 4, 0 ],
+  [ 4, 6, 0, 6, 2, 0, 1, 2 ],
+  [ 5, 7, 0, 3, 3, 6, 0, 5 ] 
 ];
 
 const t1 = Date.now();
